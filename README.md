1b4cbca6256172974ae005d1dbcc68fb0ea227cc
RogierStegeman <rogier.stegeman@upcmail.nl>
2019-02-12 11:34:20 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SigmaEnigma
-Sigma binding site prediction tool
+### Sigma binding site prediction tool
+* A sigma factor is a protein needed for initiation of transcription in bacteria.
+* An enigma is a mysterious or hard to understand person or thing.
---
+++ modules/getNoneData/getnones.py
@@ -0,0 +1,35 @@
+import random
+def main():
+    with open("genes.txt", "r") as genes:
+        with open("nonedata.txt", "w") as nonedata:
+            counter = 0
+            for line in genes.readlines():
+                a = line.split("-")
+                b = a[-1].split("	")
+                seq = b[-1]
+                if len(seq) > 80:
+                    counter += 1
+                    if counter % 3 == 0 and counter <= 1294*3:
+                    # if counter <= 1294:
+                        l = int(len(seq)/2)
+                        if counter % 2 == 0:
+                            nonedata.write(getComplement("".join(reversed(seq[l-40:l+40]))))
+                        else:    
+                            nonedata.write(seq[l-40:l+40])
+                        nonedata.write("\n")
+
+def getComplement(seq):
+    comp = ""
+    for nuc in seq:
+        if nuc == "A":
+            comp += "T"
+        elif nuc == "T":
+            comp += "A"
+        elif nuc == "C":
+            comp += "G"
+        elif nuc == "G":
+            comp += "C"
+    return comp
+
+if __name__ == "__main__":
+    main()
