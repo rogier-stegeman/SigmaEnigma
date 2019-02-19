f5c11c6e2024ca5a794fb747c6e6f614d217a61b
RogierStegeman <rogier.stegeman@upcmail.nl>
2019-02-19 12:05:40 +0100

    removed old function

--- modules/sequence_feature_start/sequence_Analysis.py
+++ modules/sequence_feature_start/sequence_Analysis.py
@@ -14,6 +14,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn import preprocessing
 
+
 def main():
     #get_data_excel()
     pandas_df = csv_to_pandad_df()
@@ -27,44 +28,7 @@ def vectorizeSequence(seq):
     ltrdict = {'a':[1,0,0,0],'c':[0,1,0,0],'g':[0,0,1,0],'t':[0,0,0,1], 'n':[0,0,0,0]}
     return np.array([ltrdict[x] for x in seq])
 
-# Old function
-"""
-def get_data_excel():
-    "Load the data from an Excel worksheet"
-    book = openpyxl.load_workbook('data/datasetWithNone.xlsx', data_only=True)
-    df = pd.DataFrame()
-    sheet = book["Sheet1"]
-    with open('data/sigma_data.csv', 'w') as csvfile:
-        csvfile.write("name,{}sigma\n".format("base,"*81))
-    for row in sheet:
-        base_list = []
-        id = row[0].value
-        sequence = row[2].value
-        for base in sequence:
-            base_list.append(base)
-        sigma = row[1].value
-        if sigma is None:
-            sigma = "Not present"
-        elif "," in sigma:
-            print("more sigma:",sigma)
-            sigma_list = sigma.split(",")
-            for sigma in sigma_list:
-                base_list = []
-                id = row[0].value
-                sequence = row[2].value
-                for base in sequence:
-                    base_list.append(base)
-                print(sigma)
-                base_list.append(str(sigma.strip("\n").strip(" ")))
-                base_list.insert(0, str(id)+str(sigma[6:]))
-                write_to_excel(base_list)
-        else:        
-            base_list.append(str(sigma.strip("\n")))
-            base_list.insert(0, str(id))
-            write_to_excel(base_list)
 
-    book.close()
-"""
 def get_data_excel():
     """Load the data from an Excel worksheet"""
     book = openpyxl.load_workbook('data/datasetWithNone.xlsx', data_only=True)
@@ -119,7 +83,7 @@ def machine_learn(df):
     seed = 7
     y = df.iloc[:,-1]
     #X = df.iloc[:, df.columns != all(['sigma','name'])]
-    X = df.iloc[:, all(df.columns != 'name' and df.columns != 'sigma')]
+    X = df.iloc[:, df.columns != 'name' and df.columns != 'sigma']
     model = RandomForestClassifier(n_jobs=-1, n_estimators=1800, max_features=0.4, max_depth=46, max_leaf_nodes=40,
                            min_samples_leaf=0.05, min_samples_split=0.2)
     #print(X)
