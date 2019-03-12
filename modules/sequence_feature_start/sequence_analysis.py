7caa15c9f74d0228d38e8c029ac4d6a1471c4ebe
Mark <damian.bolwerk@gmail.com>
2019-03-12 14:15:53 +0100

    .

--- modules/sequence_feature_start/sequence_analysis.py
+++ modules/sequence_feature_start/sequence_analysis.py
@@ -18,6 +18,7 @@ from sklearn.tree import export_graphviz
 from subprocess import call
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+import re
 
 
 def main():
@@ -166,6 +167,35 @@ def pre_process(df):
 
 
     return X,y
+
+def boxFinder(seq):
+    box10 = findRe(pattern='[TG]A[ACGT]{3}[AT]',seq=seq.upper()[35:60],start=True, add = 35)
+    box35 = findRe(pattern='T[GT]{2}[ACGT]{3}',seq=seq.upper()[:35],start=False)
+    box10Ext = False
+    afstand = 0
+    if not isinstance(box10,bool):
+        box10Ext = findRe(pattern='G[ACTG][TG]A[ACGT]{3}[AT]',seq=seq.upper()[35:60],start=True, add = 35)
+        if not isinstance(box35,bool):
+            if not isinstance(box10Ext,bool):
+                afstand = box10Ext - box35
+            else:
+                afstand = box10 - box35
+
+    return ([not isinstance(box35,bool),
+             not isinstance(box10Ext,bool),
+             not isinstance(box10,bool),
+afstand])
+
+
+def findRe(pattern, seq, start, add = 0):
+    try:
+        x = re.search(pattern,seq)
+        if start:
+            return(x.start()+add)
+        else:
+            return(x.end()+add)
+    except AttributeError as a:
+        return(False)
 
 def machine_learn(df):
     seed = 7
