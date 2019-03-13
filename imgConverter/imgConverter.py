94dbf8612f5dacc0c5f73f1eed0541debd3e632e
Alex Janse <Alex.Janse@radboudumc.nl>
2019-03-13 19:17:20 +0100

    working on imgConverter

--- imgConverter/imgConverter.py
+++ imgConverter/imgConverter.py
@@ -5,15 +5,39 @@ import os
 def imgConverter(dir = "informaticaProject"):
   first = True
   array = []
+  names = []
+  diseases = []
+  diseaseDict = getDDict()
   for filename in os.listdir(dir):
       if filename.endswith(".png"):
-        img = load_img(filename,target_size=(1024,1024))
+        img = load_img(dir+os.sep+filename,target_size=(1024,1024))
         img = img_to_array(img)
-        img = img.reshape((1,img.shape[1],img.shape[2],img.shape[0]))
+
+        names.append(filename)
+        disease = diseaseDict[filename]
+
         if first:
           first = False
           array = [img]
         else:
           array  = [array,img]
-  return array
 
+        if len(disease) > 1:
+          for i in range(2,len(disease)+1):
+            array = [array, img]
+            names.append(filename)
+
+        diseases = diseases + disease
+
+  return array, names, diseases
+
+def getDDict():
+  file = open(r"C:\Users\ajare\Dropbox\school\dataScienceProject\imgConverter\imgNamesDiseases.csv")
+  dict = {}
+  for rule in file:
+    part = rule.split(",")
+    dict[part[0]] = part[1].split("|")
+
+  return dict
+
+x, y, z = imgConverter(r"C:\Users\ajare\Dropbox\school\dataScienceProject\imgConverter")
