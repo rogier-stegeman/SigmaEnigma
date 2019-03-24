d9c6d7c04bb86e16dd6a5974f17a740da6d707b7
Mark <damian.bolwerk@gmail.com>
2019-03-24 19:17:22 +0100

    .

--- modules/sequence_feature_start/sequence_analysis.py
+++ modules/sequence_feature_start/sequence_analysis.py
@@ -1,46 +1,42 @@
+# ©Damian Bolwerk , Alex Jansen , Rogier Stegemans
+# Deze programma analyseerd sequenties en classifeerd deze met behulp van machine learning als sigma 70 bevattende sequentie of een sequentie
+# die geen sigma 70 bevat.
+
+# de nodige modules voor deze programma
 import pandas as pd
 import numpy as np
-import openpyxl
-import csv
-import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import model_selection
-from sklearn.model_selection import cross_validate
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_validate
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from sklearn import preprocessing
-from sklearn.utils import shuffle
-from sklearn.tree import export_graphviz
-from subprocess import call
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-import re
 from sklearn.model_selection import learning_curve
+from sklearn.utils import shuffle
+pd.options.mode.chained_assignment = None
 
-
+# De verschillende functies die nodig zijn om een bestand met sequenties te analyseren wordt hier aangeroepen.
 def main():
-    pandas_df = csv_to_pandad_df()
+    pandas_df = csv_to_pandas_df()
     X,y = pre_process(pandas_df)
-    #print(X)
-   # print(y)
     models = create_models()
+    #machine_learn_only(models, X, y)
     test_multiple_models(models, X, y)
 
-   # machine_learn(pandas_df)
-
-
+# leest een csv bestand in en zit het om in een pandas dataframe
 def csv_to_pandas_df():
     try:
         df = pd.read_csv('data/sigma_data.csv')
-        #print(df)
         return df
     except:
         print()
 
+
+# De verschilende machine learning algoritme die in de programma gebruikt worden
 def create_models():
         models = [
             ('LOR',
@@ -57,8 +53,10 @@ def create_models():
                                     min_samples_leaf=0.05, min_samples_split=0.2))
         ]
 
-        return model
-
+        return models
+# traint de verschilende machine learning modelen en laat verschilende testen zien
+# waaruit blijkt hoe goed elke algortime in staat is om sequenties met een siga 70
+# accuraat te classificeren.
 def test_multiple_models(models, X, y):
     # test multiple models. Optional function
     results = []
@@ -70,23 +68,21 @@ def test_multiple_models(models, X, y):
     min = []
     f1 = []
     recall = []
-    precicion = []
+    precision = []
     cm = []
     for name, model in models:
         print(name)
+        model.fit(X, y)
         if name == "RF":
-            model.fit(X, y)
-            curve = plot_learning_curve(model,name,X,y)
-            curve.show()
-            show_tree(model, X, y)
             feature_selection(model, X)
+            curve = plot_learning_curve(model, name, X, y)
+            curve.show()
         kfold = model_selection.KFold(n_splits=4, random_state=seed)
         accuracy_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring="accuracy")
         F1_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring="f1")
         recall_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring="recall")
-        precicion_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring="average_precision")
+        precision_results = model_selection.cross_val_score(model, X, y, cv=kfold, scoring="average_precision")
         y_pred = model_selection.cross_val_predict(model, X, y, cv=kfold)
-
         y2 = y
         confusion = confusion_matrix(y2, y_pred)
         visualize_confusion_matrix(confusion,name)
@@ -96,7 +92,7 @@ def test_multiple_models(models, X, y):
         names.append(name)
         f1.append(F1_results.mean())
         recall.append(recall_results.mean())
-        precicion.append(precicion_results.mean())
+        precision.append(precision_results.mean())
         mean.append(accuracy_results.mean())
         std.append(accuracy_results.std())
         max.append(accuracy_results.max())
@@ -109,15 +105,29 @@ def test_multiple_models(models, X, y):
     ax.set_xticklabels(names)
     plt.show()
 
+# Deze functie traint alleen de modellen en laat minimale test resultaten zien en heeft daarom kortere runtime.
+def machine_learn_only(model,x,y):
+     for name, model in model:
+         seed = 7
+         model = model.fit(x, y)
+         kfold = model_selection.KFold(n_splits=4, random_state=seed)
+         scoring = {"accuracy": "accuracy",
+                     "f1": "f1",
+                     "recall":"recall",
+                    "average_precision":"average_precision",
+                     }
+         scores = cross_validate(model, x, y, cv=kfold, scoring=scoring,
+                                 return_train_score=False)
+         for k, v in scores.items():
+             print(k, v)
+
+# Verwerkt de data van de pandas dataframe zodanig dat het gelezen kan worden door de machine
+# learning algortime
 def pre_process(df):
     colsy = [col for col in df.columns if col in ['sigma']]
-    print(colsy)
     colsx = [col for col in df.columns if col not in ['name', 'sigma']]
-    print("he")
     X = df[colsx]
     pre_y = df[colsy]
-    print(X)
-    print(pre_y)
     base_dict = {
         "a": 0,
         "c": 1,
@@ -126,48 +136,43 @@ def pre_process(df):
     }
     for col in X.columns:
         if not col.startswith("box"):
-            print(col)
-            X[col] = X[col].map(base_dict)
+
+            X[col] =X[col].map(base_dict)
 
     label_encoder = LabelEncoder()
     integer_encoded_label = label_encoder.fit_transform(pre_y.values.ravel())
     integer_encoded_label = integer_encoded_label.reshape(len(integer_encoded_label), 1)
     y = integer_encoded_label.ravel()
-
-
+    X, y = shuffle(X,y, random_state=0)
 
     return X,y
 
+# Laat de belang van elke feature zien waaruit de decision tress konden worden opgebouwd.
 
 def feature_selection(model,X):
-    print(type(X))
-
     feature_importances = pd.DataFrame(model.feature_importances_,
                                        index=X.columns,
                                        columns=['importance']).sort_values('importance', ascending=False)
     print(feature_importances)
+    x_label = list( feature_importances.index)
+
     importances = model.feature_importances_
-    print(model.classes_)
     std = np.std([model.feature_importances_ for tree in model.estimators_],
                  axis=0)
     indices = np.argsort(importances)[::-1]
-
-    # Print the feature ranking
-    print("Feature ranking:")
-
-    for f in range(X.shape[1]):
-        print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
 
     # Plot the feature importances of the forest
     plt.figure()
     plt.title("Feature importances")
     plt.bar(range(X.shape[1]), importances[indices],
             color="r", yerr=std[indices], align="center")
-    plt.xticks(range(X.shape[1]), indices)
+    plt.xticks(range(X.shape[1]), x_label,rotation=90)
     plt.xlim([-1, X.shape[1]])
+    
     plt.show()
 
-
+# Laat zien hoe de verdeling is tussen de voorspellingen van de machine learning model
+# en de daadwerkelijk klassen.
 
 def visualize_confusion_matrix( cm, name):
     plt.clf()
@@ -185,29 +190,8 @@ def visualize_confusion_matrix( cm, name):
             plt.text(j, i, str(s[i][j]) + " = " + str(cm[i][j]))
     plt.show()
 
-def show_tree(model,X,y):
-    #Export as dot file
-    print(model)
-    estimator = model.estimators_[5]
-
-
-    print(list(X.columns))
-#    print(list(y.columns))
-    export_graphviz(estimator, out_file='tree.dot',
-                feature_names=list(X.columns),
-                class_names=['none','Sigma70'],
-                rounded=True, proportion=False,
-                precision=2, filled=True)
-
-    os.environ['PATH'] = os.getcwd()+"\\tree.dot"
-    print("hier")
-    print(os.environ['PATH'])
-
-# Convert to png using system command (requires Graphviz)
-
-    print(os.getcwd())
-    call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png'])
-
+# Laat zien hoe goed de machine learning model presteerd op de trainings data en de validatie data naarmate
+# de data waarop het getraind wordt groter wordt.
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
     plt.figure()
     plt.title(title)
